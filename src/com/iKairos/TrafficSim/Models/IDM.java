05328dcec52c3af1ce28ef13fbe406a10905b1fb
wanderanimrod <wanderanimrod@gmail.com>
2013-03-03 13:41:25 +0530

    Cleaned IDM and LaneChangeModel of comments and unused debug print code

--- src/com/iKairos/TrafficSim/Models/IDM.java
+++ src/com/iKairos/TrafficSim/Models/IDM.java
@@ -4,96 +4,31 @@ import com.iKairos.TrafficSim.Agents.Vehicle;
 import com.iKairos.TrafficSim.Simulation;
 
 /**
- * This class holds the properties and methods of the Intelligent Driver Model due to Treiber et al.
- * <br>The model's main function is to determine the acceleration of a vehicle given a set of parameters
- * about its current condition of speed and the condition of its leading vehicle
- * The IDM used is as defined here: <a href = "http://en.wikipedia.org/wiki/Intelligent_driver_model">
- * M. Treiber et al., Intelligent Driver Model </a>
- * <br> An instance of this object should be created once at the start of the simulation and reused by all
- * subsequent calls to the method calculateAcceleration() to save computational resources.
- * @author Nimrod
+ * Based on the Intelligent Driver Model by Treiber et al.
+ * Variable names used are as described in the Intelligent Driver Model Wikipedia entry
  */
 public class IDM {
 
-		/*
-		 * The variable names used here are as described in the Wikipedia article on IDM
-		 * Here are the default values of the parameters that don't vary among vehicles as used in the calculation
-		 */
-
     private double T;
-
     private double delta;
-
     private double so;
 
-    /**
-     * Initialises global IDM parameters to their default values
-     * These can be adjusted using the setters in this class.
-     */
     public IDM() {
-
         T = Simulation.SimConstants.safeTimeHeadway;
         delta = Simulation.SimConstants.accelerationExponent;
         so = Simulation.SimConstants.minJamDistance;
     }
 
-    /**
-     * Calculates the acceleration of the client vehicle using the IDM.
-     *
-     * @param leader The vehicle ahead of the requester vehicle
-     * @param requester The vehicle whose acceleration to calculate
-     * @return The acceleration of the requester vehicle
-     */
     public double calculateAcceleration (Vehicle leader, Vehicle requester) {
 
-        if (requester.getId() == 1) {
-            //u.println("Initial vehicle parameters (" + vehicle.getId() + "):");
-        }
         double v = requester.getVelocity();
-        if (requester.getId() == 1) {
-            //u.printlntb("Velocity = " + v);
-        }
         double a = requester.getMaxAcceleration();
-        if (requester.getId() == 1) {
-            //u.printlntb("Max Acc = " + a);
-        }
         double b = requester.getDesiredDeceleration();
-        if (requester.getId() == 1) {
-            //u.printlntb("Desired Dec = " + b);
-        }
         double deltaV = v - leader.getVelocity();
-        if (requester.getId() == 1) {
-
-            //u.printlntb("Leader vel = " + leading.getVelocity());
-            //u.printlntb("Leader pos = " + leading.getPosition());
-            //u.printlntb("DeltaV = " + deltaV);
-        }
         double s = leader.getPosition() - requester.getPosition() - leader.getLength();
-			/*u.println("leaderPos (" + leader.getPosition() + "), id (" + leader.getId() + ") - requesterPos (" + requester.getPosition()
-					+ ", id (" + requester.getId() + ") - leaderLen (" + leader.getLength() + ")\n");*/
-
-			/*//Debug
-			if (leader.getVelocity() == 0.0d & s < 200) {
-				u.println("The clearance from static leader = " + s + " and my current position = " + requester.getPosition() + "\n");
-				//throw new IllegalAccessError("\n\nWarning: Vehicle " + requester.getId() + " approaching static vehicle " + leader.getId());
-			}*/
-
-        if (requester.getId() == 1) {
-            //u.printlntb("Gap s = " + s);
-            //u.printlntb("Min Gap So = " + so);
-            //u.printlntb("Delta = " + delta);
-            //u.printlntb("T = " + T);
-        }
-
         double sStar = so + (v*T) + ((v*deltaV) / (2*Math.sqrt(a*b)));
-        if (requester.getId() == 1) {
-            //u.printlntb("sStar = " + sStar);
-        }
-        double acceleration = a * (1 - Math.pow((v/requester.getDesiredVelocity()), delta) - (Math.pow((sStar/s), 2)));
 
-        if (acceleration < -Simulation.SimConstants.maxDeceleration) {
-            //acceleration = -SimConstants.maxDeceleration;
-        }
+        double acceleration = a * (1 - Math.pow((v/requester.getDesiredVelocity()), delta) - (Math.pow((sStar/s), 2)));
 
         return acceleration;
     }
--- src/com/iKairos/TrafficSim/Models/LaneChangeModel.java
+++ src/com/iKairos/TrafficSim/Models/LaneChangeModel.java
@@ -6,20 +6,11 @@ import com.iKairos.TrafficSim.Network.Lane;
 import com.iKairos.TrafficSim.Simulation;
 
 /**
- * This class holds the logic and data for making lane change decisions
- * At the center of its function is the
- * <a href="http://141.30.51.183/~treiber/publications/MOBIL.pdf">
- * MOBIL lane changing model due to Treiber et al.</a>
- * <br>For lane changing decisions on two-way roads, a modified MOBIL model due to <u>Wandera Nimrod</u>
- * is employed.
- */
+ * Based on the MOBIL lane change model due to Treiber et al.
+ * Variables and Logic used as described in the Wikipedia entry on MOBIL*/
+//TODO Adjust this Mode or create another one that deals with Overtaking on rural roads
 public class LaneChangeModel {
 
-    /**
-     * This method uses <i>MOBIL</i> to determine if the requesting vehicle needs to change lane or not.
-     * @param requester The vehicle inquiring about its lane change need.
-     * @return Whether or not requester should change lane.
-     */
     public void ChangeLaneIfNecessary (Vehicle requester) {
 
         Lane currentLane = requester.getCurrentLane();
@@ -30,8 +21,6 @@ public class LaneChangeModel {
         else if (currentLane.getParentEdge().getType() == EdgeType.ONE_WAY_MULTIPLE_LANES) {
 
             Lane targetLane = currentLane.getNextLane();
-
-            //Then, deal with lane changes on one-way edges, starting with two-lane edges first
 
             //TODO Check thread safety for these calculations.
 
@@ -44,43 +33,38 @@ public class LaneChangeModel {
             double accNewFollowerAfterLaneChange = 0.0d;
 
             if (prospectiveFollower != null) {
-
                 accNewFollowerBeforeLaneChange = prospectiveFollower.getAcceleration();
                 accNewFollowerAfterLaneChange = Simulation.SimConstants.idm.calculateAcceleration(requester, prospectiveFollower);
             }
             //Else leave the acceleration at 0 so it plays no part in the lane changing decision.
 
             if (currentFollower != null) {
-
                 accCurrentFollowerBeforeLaneChange = currentFollower.getAcceleration();
                 accCurrentFollowerAfterLaneChange = Simulation.SimConstants.idm.calculateAcceleration(currentLane.getLeadingVehicle(requester), currentFollower);
             }
 
+            //TODO Refactor this chain stuff out to conform to the law or Demeter
             double accRequesterBeforeLaneChange = requester.getAcceleration();
             double accRequesterAfterLaneChange = Simulation.SimConstants.idm.calculateAcceleration(targetLane.getProspectiveLeadingVehicle(requester), requester);
             double incentiveCriterion = accRequesterAfterLaneChange - accRequesterBeforeLaneChange
                     + requester.getPoliteness() * (accNewFollowerAfterLaneChange - accNewFollowerBeforeLaneChange
                     + accCurrentFollowerAfterLaneChange - accCurrentFollowerBeforeLaneChange);
 
-            //u.println("Incentive to change lane for Vehicle \"" + requester.getId() + "\" = " + incentiveCriterion + "\n");
-
+            //TODO Put this clearance check in another method
             if (incentiveCriterion > Simulation.SimConstants.laneChangeThreshold) {
-
-                //Ensure that the lane change will leave enough clearance between requester and new follower.
                 if (prospectiveFollower != null) {
 
                     if (requester.getPosition() - prospectiveFollower.getPosition() - requester.getLength() >= Simulation.SimConstants.minJamDistance) {
-                        //Change Lane to target lane
                         requester.changeLane(targetLane);
                     }
-                    //Else there will not be enough clearance from the new follower after the lane change
+                    //Else there will not be enough clearance from the new follower
                 }
                 else {
                     //No new follower. Change Lane to target lane
                     requester.changeLane(targetLane);
                 }
             }
-            //Else do nothing. Leave the vehicle in its current lane.
+            //Else leave the vehicle in its current lane
         }
     }
 }
