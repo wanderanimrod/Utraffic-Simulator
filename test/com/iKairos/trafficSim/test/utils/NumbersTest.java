87ebd8e0e13085014b66fb2131e923aeb76ff254
wanderanimrod <wanderanimrod@gmail.com>
2014-04-03 15:51:40 +0300

    Tested that number rounding works

---
+++ test/com/iKairos/trafficSim/test/utils/NumbersTest.java
@@ -0,0 +1,25 @@
+package com.iKairos.trafficSim.test.utils;
+
+import com.iKairos.utils.Numbers;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class NumbersTest {
+    @Test
+    public void shouldRoundOffNumberByRaisingLastDigitWhenTrailingDecimalIsGreaterThan5() {
+        double rounded = Numbers.round(0.7336, 3);
+        assertThat(rounded, is(0.734));
+    }
+    @Test
+    public void shouldRoundOfNumberByTruncatingWhenTrailingDecimalIsLessThan5() {
+        double rounded = Numbers.round(0.7334, 3);
+        assertThat(rounded, is(0.733));
+    }
+    @Test
+    public void shouldRoundOfNumberByRaisingLastDigitWhenTrailingDecimalIs5() {
+        double rounded = Numbers.round(0.7335, 3);
+        assertThat(rounded, is(0.734));
+    }
+}
